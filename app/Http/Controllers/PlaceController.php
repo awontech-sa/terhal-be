08d70980efbfff9563120738f845d3fba81ff7ae
xilujain <lujainboo@gmail.com>
2025-01-14 12:30:03 +0300

    refactor: update PlaceController index method to return all places instead of paginated collection

--- app/Http/Controllers/PlaceController.php
+++ app/Http/Controllers/PlaceController.php
@@ -13,9 +13,9 @@ class PlaceController extends Controller
      */
     public function index()
     {
-        $places = Place::paginate(10);
+        $places = Place::all();
 
-        return PlaceResource::collection($places);
+        return $places;
     }
 
     /**
