32d1d42431513e41eb8e601a82a04927456a2991
xilujain <lujainboo@gmail.com>
2025-01-15 15:20:36 +0300

    refactor: simplify EventResource by replacing individual fields with a collection

--- app/Http/Resources/TouristResources/EventResource.php
+++ app/Http/Resources/TouristResources/EventResource.php
@@ -16,20 +16,9 @@ class EventResource extends JsonResource
     {
         return [
             'event_owner' => $this->user->name,
-            'event_id' => $this->id,
-            'e_name' => $this->e_name,
-            'e_description' => $this->e_description,
-            'e_images' => $this->e_images,
-            'e_videos' => $this->e_videos,
-            'e_duration' => $this->e_duration,
-            'e_location' => $this->e_location,
-            'e_price' => $this->e_price,
-            'e_rate' => $this->e_rate,
-            'e_date' => $this->e_date,
+            'event' => EventResource::collection($this->id),
             'event_type_id' => $this->eventType->id,
             'event_type' => $this->eventType->et_name,
-            'e_lang' => $this->e_lang,
-            'e_lat' => $this->e_lat,
             'attendees' => AttendeeResource::collection($this->attendees)
         ];
     }
