80e7c8f980d1d3d25069f4044c4e446abdbd3efb
xilujain <lujainboo@gmail.com>
2025-01-22 11:58:28 +0300

    refactor: update TourResource to decode t_places from JSON for improved data handling

--- app/Http/Resources/TouristResources/TourResource.php
+++ app/Http/Resources/TouristResources/TourResource.php
@@ -27,6 +27,7 @@ class TourResource extends JsonResource
             'date' => $this->t_date,
             'price' => $this->t_price,
             'participants' => ParticipantResource::collection($this->participants),
+            'places' => json_decode($this->t_places, true)
         ];
     }
 }
