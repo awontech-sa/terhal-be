388dc71019460acb541c5d499243b4b834e3040f
xilujain <lujainboo@gmail.com>
2025-03-18 15:49:05 +0300

    fix: add import for TourResource in UserTourResource

--- app/Http/Resources/TouristResources/UserTourResource.php
+++ app/Http/Resources/TouristResources/UserTourResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\TouristResources;
 
+use App\Http\Resources\TourGuideResources\TourResource;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
