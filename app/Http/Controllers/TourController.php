2149308264f20c356b23279489352a183cb14bf6
xilujain <lujainboo@gmail.com>
2025-04-21 18:57:17 +0300

    fix: update bookingShow method to return raw booking data and restore tour_id and tour resource in UserTourResource

--- app/Http/Controllers/TourController.php
+++ app/Http/Controllers/TourController.php
@@ -176,7 +176,7 @@ class TourController extends Controller
 
             return response()->json([
                 'message' => 'Booking details retrieved successfully.',
-                'data' => UserTourResource::collection($booking)
+                'data' => $booking
             ], 200);
         } catch (\Exception $e) {
             return response()->json([
