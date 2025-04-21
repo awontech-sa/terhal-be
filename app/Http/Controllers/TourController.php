51acb7f1b277130cdf3caa8298c32295f8aa9d36
xilujain <lujainboo@gmail.com>
2025-04-21 19:07:38 +0300

    fix: update bookingShow method to return a query builder for booking instead of a single booking

--- app/Http/Controllers/TourController.php
+++ app/Http/Controllers/TourController.php
@@ -158,7 +158,7 @@ class TourController extends Controller
         $user = Auth::user();
 
         try {
-            $booking = UserTour::where('user_id', $user->id)->with('tour')->first();
+            $booking = UserTour::where('user_id', $user->id)->with('tour');
 
             if (!$booking) {
                 return response()->json([
